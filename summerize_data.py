962d8bef26ccd29cb8be0d12dad999e7e92128c0
sgsuper12373 <sgsuper12373@gmail.com>
2025-04-26 02:11:46 +0530

    covariance is done and improved some logic\n remaining is distribution identification.

--- summerize_data.py
+++ summerize_data.py
@@ -3,9 +3,34 @@ file_path = "Grouped_Imports_by_Group_All_Years.csv"
 df = pd.read_csv(file_path)
 
 #stroing quntities and values of each year in array
+def covariance(X,Y):
+    n = len(X)
+    mean_X = sum(X) / n
+    mean_Y = sum(Y) / n
+    cov = sum((X[i] - mean_X) * (Y[i] - mean_Y) for i in range(n)) / n
+    return cov
+def correlation_coefficient(X, Y):
+    n = len(X)
+    mean_X = sum(X) / n
+    mean_Y = sum(Y) / n
+    cov = covariance(X, Y)
+    std_X = (sum((X[i] - mean_X) ** 2 for i in range(n)) / n) ** 0.5
+    std_Y = (sum((Y[i] - mean_Y) ** 2 for i in range(n)) / n) ** 0.5
+    return cov / (std_X * std_Y)
+
+def average(lst):
+    return round(sum(lst) / len(lst),3)
+
+def std_div(lst):
+    n = len(lst)
+    mean = average(lst)
+    variance = sum((x - mean) ** 2 for x in lst) / (n - 1)
+    return round(variance ** 0.5,3)
+
 Quanties_year_array= []
 Value_year_array = []
 years = ["2014-15", "2015-16", "2016-17", "2017-18", "2018-19", "2019-20", "2020-21", "2021-22"]
+
 for year in years:
     qty_lst = df[f"{year} QTY"]
     val_lst = df[f"{year} VAL"]
@@ -14,41 +39,28 @@ for year in years:
 
 Avarage_Quanties_year_array = []
 for i in range(len(Quanties_year_array)):
-    sum = 0 
-    for j in range(len(Quanties_year_array[i])):
-        sum += Quanties_year_array[i][j]
-    Avg = round(sum/len(Quanties_year_array[i]),3) #rounding to 3 decimal points
-    Avarage_Quanties_year_array.append(Avg)
-    # Avarage_Quanties_year_array.append(sum/len(Quanties_year_array[i]))
-
+    Avarage_Quanties_year_array.append(average(Quanties_year_array[i]))
 Avarage_value_year_array = []
 for i in range(len(Value_year_array)):
-    sum = 0 
-    for j in range(len(Value_year_array[i])):
-        sum+=Value_year_array[i][j]
-    Avg = round(sum/len(Value_year_array[i]),3) #rounding to 3 decimal points
-    Avarage_value_year_array.append(Avg)
-    # Avarage_value_year_array.append(sum/len(Value_year_array[i]))
+    Avarage_value_year_array.append(average(Value_year_array[i]))
 
-print("Average Quantities by year : " ,Avarage_Quanties_year_array);
-print("Average Values by year : " ,Avarage_value_year_array);
+print("Average Quantities by year : " ,Avarage_Quanties_year_array)
+print("Average Values by year : " ,Avarage_value_year_array)
 
 '''Calculating standard deviation fo each year'''
 std_div_quantiy = []
 std_div_value = []
 for i in range(len(Quanties_year_array)):
-    sum = 0
-    for j in range(len(Quanties_year_array[i])):
-        sum+=(Quanties_year_array[i][j] - Avarage_Quanties_year_array[i])**2
-    std_div = round((sum/ (len(Quanties_year_array[i])) -1) ** 0.5,3) #rounding to 3 decimal points
-    std_div_quantiy.append(std_div)
-
+    std_div_quantiy.append(std_div(Quanties_year_array[i]))
 for i in range(len(Value_year_array)):
-    sum = 0
-    for j in range(len(Value_year_array[i])):
-        sum+=(Value_year_array[i][j] - Avarage_value_year_array[i])**2
-    std_div = round( (sum / (len(Value_year_array[i])-1)) ** 0.5,3) #rounding to 3 decimal points
-    std_div_value.append(std_div)
+    std_div_value.append(std_div(Value_year_array[i]))
 
-print("Standard Deviation of Quantities by year : " ,std_div_quantiy);
-print("Standard Deviation of Values by year : " ,std_div_value);
+print("Standard Deviation of Quantities by year : " ,std_div_quantiy)
+print("Standard Deviation of Values by year : " ,std_div_value)
+
+'''Calculating correlation coefficient of total quantities and toatal values'''
+Total_Quantities_per_year = [sum(Quanties_year_array[i]) for i in range(len(Quanties_year_array))]
+Total_Value_per_year = [sum(Value_year_array[i]) for i in range(len(Value_year_array))]
+
+corr= round(correlation_coefficient(Total_Quantities_per_year , Total_Value_per_year), 3);
+print("Correlation coefficinet between Total Quatities from year 2014->2021 and Total Values from year 2014->2021 : ", corr)
